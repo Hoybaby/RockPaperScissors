c8f2517c797c7f40e1b46ac05e0c8af0ec4863af
Hoybaby <mbartek436@gmail.com>
2021-02-02 15:37:12 -0500

    getting my early code working to grab and start the game with the intro screen fading and presenting the option to be pressed

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const game = () => {
     let pScore = 0;
     let cScore = 0;
 
-    
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const matchScreen = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut'); // this adds a class to the intro class we targeted with querySelector. it adds the class fadeOut on click!
+        })
+    }
 
 }

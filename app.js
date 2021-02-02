5fa14b18f1be6b0c00628755571a81e402a1b878
Hoybaby <mbartek436@gmail.com>
2021-02-02 15:56:05 -0500

    cleaning some code and grabbing the html components with document.querySelector to grab the values. Also very important to mark what things are doing with comments so at a later date I can understand what some of these are as well as other coders looking at my little project

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const game = () => {
     let pScore = 0;
     let cScore = 0;
 
+    //this will start the game.
     const startGame = () => {
         const playBtn = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
@@ -12,9 +13,16 @@ const game = () => {
 
         playBtn.addEventListener('click', () => {
             introScreen.classList.add('fadeOut'); // this adds a class to the intro class we targeted with querySelector. it adds the class fadeOut on click!
+            matchScreen.classList.add('fadeIn');
         })
     }
 
+    const playMatch = () => {
+        const options = document.querySelector('.options button') //this will grab all the buttons from the options class on line 40
+    }
+
+
+    //calls all the inner functions
     startGame();
 }
 

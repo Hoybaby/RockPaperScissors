0a448f43320dd0ac0098d729ce49c4aceb3d4b4c
Hoybaby <mbartek436@gmail.com>
2021-02-03 11:31:11 -0500

    making the number generator so the computer will randomly pick

--- app.js
+++ app.js
@@ -19,11 +19,21 @@ const game = () => {
 
     const playMatch = () => {
         const options = document.querySelector('.options button') //this will grab all the buttons from the options class on line 40
-    }
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+
+        //computer Hands/choices to fight against player
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        const computerNumber = Math.random() * 3;
+        console.log(computerNumber);
+        
+    };
 
 
     //calls all the inner functions
     startGame();
+    playMatch();
 }
 
 //start the game function
